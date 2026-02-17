22611f1b0a90185a22305d0aece7f10b3b437ec4
titus-79 <58149956+titus-79@users.noreply.github.com>
2026-02-17 14:32:14 +0100

    feat :add assets class (#10)

--- src/main.java
+++ src/main.java
@@ -4,7 +4,8 @@ class Main {
     public static void main(String[] args) {
         boolean victoire = false;
         int round = 0;
-        while (habitant > 0 && victoire == false) {
+        Assets player = new Assets(0,0, 50, 100, 1);
+        while (player.citizens > 0 && victoire == false) {
 
             Scanner scanner = new Scanner(System.in);
             menu(scanner);
