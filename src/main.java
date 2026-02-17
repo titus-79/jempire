87c9445b04c16591c9c3fa3b04f37b3502d1cc52
titus-79 <titus@ik.me>
2026-02-17 13:47:55 +0100

    creation de la boucle principale

--- src/main.java
+++ src/main.java
@@ -1,6 +1,15 @@
-class Main{
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-    
+        boolean victoire = false;
+        int round = 0;
+        while (habitant > 0 && victoire == false) {
+
+            Scanner scanner = new Scanner(System.in);
+            menu(scanner);
+            scanner.close();
+        }
     }
 
 }
